d8a7518a2a0852a79bfee08a5728e7a8ff39602b
Mateus R Domingos <90119765+ninjamrd@users.noreply.github.com>
2023-02-01 09:25:43 -0300

    Update exercicio 1.cs

--- exercicio 1.cs
+++ exercicio 1.cs
@@ -23,7 +23,7 @@ namespace ex_1
                 Console.Write($"{i}°:");
                 a = Convert.ToInt32(Console.ReadLine());
 
-                if (i == 1 || a > m) m = a;
+                if (a > m || i == 1) m = a;
                 t = t + a;
 
             }
