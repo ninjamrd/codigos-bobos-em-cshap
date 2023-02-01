b61e3ebd3264be50c3ded4acaa107ea020a181a9
Mateus R Domingos <90119765+ninjamrd@users.noreply.github.com>
2023-01-31 21:41:59 -0300

    Create tabuada.cs

---
+++ tabuada.cs
@@ -0,0 +1,26 @@
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n, i;
+
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            for(i=0; i<=10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n * i}");
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
